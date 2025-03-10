6a81af26b639b9afcfc573fd8453fdfdb05be961
Mahmoud Yasser <mahmoud.yasser@dija.money>
2025-03-10 19:10:14 +0200

    feat(test): add jest configuration and test setup

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/tests/**/*.test.ts', '**/test/**/*.test.ts'],
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/',
+    '/test/',
+    '/tests/'
+  ]
+};
