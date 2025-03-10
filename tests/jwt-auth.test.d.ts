ded4af1772a73c1ff7b651f7cdce562c8ca40d75
Mahmoud Yasser <mahmoud.yasser@dija.money>
2025-03-10 20:06:31 +0200

    test: add comprehensive JWT authentication middleware tests

---
+++ tests/jwt-auth.test.d.ts
@@ -0,0 +1,7 @@
+/**
+ * JWT Authentication Tests
+ *
+ * This test file verifies the functionality of the JWT authentication middleware.
+ */
+export {};
+//# sourceMappingURL=jwt-auth.test.d.ts.map
