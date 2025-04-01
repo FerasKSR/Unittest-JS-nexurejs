5d276592149855151a17c24406a80a50299319e0
Mahmoud Yasser <mahmoud.yasser@dija.money>
2025-04-02 01:24:22 +0200

    fix: update GCC diagnostic settings in simdjson_wrapper to suppress additional warnings, improving code clarity and maintainability

--- src/native/json/simdjson_wrapper.cpp
+++ src/native/json/simdjson_wrapper.cpp
@@ -5,12 +5,21 @@
  */
 
 // Disable specific warnings for simdjson
+#ifdef __GNUC__
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wbitwise-instead-of-logical"
-#pragma GCC diagnostic ignored "-Wambiguous-reversed-operator"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
 
 // Include the original simdjson implementation
 #include "../../../node_modules/simdjson/simdjson/src/simdjson.cpp"
 
 // Restore warning settings
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
