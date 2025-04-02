c0330a09908d15dea9848cb860aa4876468483e5
Mahmoud Yasser <mahmoud.yasser@dija.money>
2025-04-03 01:02:22 +0200

    chore: configure ESLint to ignore benchmarks and update CI workflows for streamlined testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json', './tsconfig.benchmarks.json'],
     tsconfigRootDir: __dirname,
     ecmaVersion: 2020,
     sourceType: 'module',
